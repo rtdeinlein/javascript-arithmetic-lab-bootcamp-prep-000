43b63d356a2847eea92672f26a2e06206b86ee0e
Reid T Deinlein <rtdeinlein5@gmail.com>
2020-08-07 16:26:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+var number;
+function add (a, b){
+  number = a + b;
+  return number;
+}
+function subtract (a, b){
+  number = a - b;
+  return number;
+}
+function multiply (a, b){
+  number = a * b;
+  return number;
+}
+function divide (a, b){
+  number = a / b;
+  return number;
+}
 
